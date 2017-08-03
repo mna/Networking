dfedb200c9885e72ccf4539ee5116563562a72b8
Martin Angers <martin.n.angers@gmail.com>
2017-08-02 21:53:21 -0400

    rename package to Networking

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Sockets",
+    name: "Networking",
     targets: [
       Target(name: "Libc"),
       Target(name: "LowSockets", dependencies: ["Libc"]),
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import SocketsTests
-
-XCTMain([
-    testCase(SocketsTests.allTests),
-])
