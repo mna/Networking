bc49b029f9c5714a27afc97ded001bdc49d7ce24
Martin Angers <martin.n.angers@gmail.com>
2017-08-02 23:12:07 -0400

    LowSockets: API to get and set options and flags

--- Sources/LowSockets/Socket.swift
+++ Sources/LowSockets/Socket.swift
@@ -1,8 +1,10 @@
 import Libc
+import Foundation
 
-// to avoid ambiguity in Socket.close (where close refers to the Socket method
-// instead of the libc/darwin system call).
+// to avoid ambiguity between the Socket methods and the libc/darwin system calls.
 private var cclose = close
+private var clisten = listen
+private var cshutdown = shutdown
 
 // MARK: - Socket
 
@@ -16,6 +18,31 @@ class Socket {
     return v
   }
 
+  private static func setOption(fd: Int32, option: Int32, value: Int32) throws {
+    var v = value
+    let ret = setsockopt(fd, SOL_SOCKET, option, &v, socklen_t(MemoryLayout<Int32>.size))
+    try Error.makeAndThrow(fromReturnCode: ret)
+  }
+
+  private static func setTimevalOption(fd: Int32, option: Int32, t: TimeInterval) throws {
+    var val = timeval()
+
+    // see https://stackoverflow.com/a/28872601/1094941
+    if t > 0 {
+      val.tv_sec = Int(t)
+
+      let us = Int(t.truncatingRemainder(dividingBy: 1) * 1_000_000)
+      #if os(Linux)
+				val.tv_usec = Int(us)
+			#else
+				val.tv_usec = Int32(us)
+			#endif
+    }
+
+		let ret = setsockopt(fd, SOL_SOCKET, option, &val, socklen_t(MemoryLayout<timeval>.stride))
+    try Error.makeAndThrow(fromReturnCode: ret)
+  }
+
   private static func setFcntl(fd: Int32, flag: Int32) throws {
     let flags = fcntl(fd, F_GETFL)
     try Error.makeAndThrow(fromReturnCode: flags)
@@ -27,13 +54,26 @@ class Socket {
     try Error.makeAndThrow(fromReturnCode: ret)
   }
 
+  // MARK: - ShutdownMode
+
+  struct ShutdownMode {
+    let value: Int32
+
+    private init(value: Int32) {
+      self.value = value
+    }
+
+    static let read = ShutdownMode(value: SHUT_RD)
+    static let write = ShutdownMode(value: SHUT_WR)
+    static let readWrite = ShutdownMode(value: SHUT_RDWR)
+  }
+
   // MARK: - Properties
 
   let fd: Int32
   let family: Family
   let type: SocketType
   let proto: SocketProtocol
-  private(set) var isBlocking: Bool = true // blocking by default
 
   // MARK: - Constructors
 
@@ -51,11 +91,11 @@ class Socket {
     self.fd = fd
 
     #if os(Linux)
-    self.family = Family.make(try Socket.getOption(fd: fd, option: SO_DOMAIN))
-    self.proto = SocketProtocol.make(try Socket.getOption(fd: fd, option: SO_PROTOCOL))
+      self.family = Family.make(try Socket.getOption(fd: fd, option: SO_DOMAIN))
+      self.proto = SocketProtocol.make(try Socket.getOption(fd: fd, option: SO_PROTOCOL))
     #else
-    self.family = .unknown
-    self.proto = .unknown
+      self.family = .unknown
+      self.proto = .unknown
     #endif
 
     self.type = SocketType.make(try Socket.getOption(fd: fd, option: SO_TYPE))
@@ -67,6 +107,29 @@ class Socket {
 
   // MARK: - Methods
 
+  func setOption(_ option: Int32, to value: Int) throws {
+    try Socket.setOption(fd: fd, option: option, value: Int32(value))
+  }
+
+  func getOption(_ option: Int32) throws -> Int {
+    return Int(try Socket.getOption(fd: fd, option: option))
+  }
+
+  // TODO: getRead/WriteTimeout, getBlocking? Read from system call, could be
+  // changed via FD outside this class.
+
+  func setReadTimeout(_ t: TimeInterval) throws {
+    try Socket.setTimevalOption(fd: fd, option: SO_RCVTIMEO, t: t)
+  }
+
+  func setWriteTimeout(_ t: TimeInterval) throws {
+    try Socket.setTimevalOption(fd: fd, option: SO_SNDTIMEO, t: t)
+  }
+
+  func setLinger() throws {
+    // TODO: implement...
+  }
+
   func setBlocking() throws {
     try Socket.setFcntl(fd: fd, flag: -O_NONBLOCK)
   }
@@ -75,9 +138,18 @@ class Socket {
     try Socket.setFcntl(fd: fd, flag: O_NONBLOCK)
   }
 
+  func listen(backlog: Int) throws {
+    let ret = clisten(fd, Int32(backlog))
+    try Error.makeAndThrow(fromReturnCode: ret)
+  }
+
+  func shutdown(mode: ShutdownMode = .readWrite) throws {
+    let ret = cshutdown(fd, mode.value)
+    try Error.makeAndThrow(fromReturnCode: ret)
+  }
+
   func close() throws {
     let ret = cclose(fd)
     try Error.makeAndThrow(fromReturnCode: ret)
   }
 }
-
