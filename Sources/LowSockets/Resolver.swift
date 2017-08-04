2a9b496434703a11d65055013e4c8e6e4fcd347c
Martin Angers <martin.n.angers@gmail.com>
2017-08-04 11:34:43 -0400

    LowSockets: implement Resolver.join and .split, test

--- Sources/LowSockets/Resolver.swift
+++ Sources/LowSockets/Resolver.swift
@@ -5,9 +5,9 @@ import Libc
 struct Resolver {
   private init() { }
 
-  // Combines host and port into a network address of the form
-  // "host:port" or "[host]:port" if the host contains a colon
-  // or a percent sign.
+  /// Combines host and port into a network address of the form
+  /// "host:port" or "[host]:port" if the host contains a colon
+  /// or a percent sign.
   static func join(host: String, port: String) -> String {
     if host.contains(":") || host.contains("%") {
       return "[\(host)]:\(port)"
@@ -15,8 +15,66 @@ struct Resolver {
     return "\(host):\(port)"
   }
 
+  /// Splits a network address of the form "host:port", "[host]:port"
+  /// or "[ipv6-host%zone]:port" into host or ipv6-host%zone and port.
   static func split(hostPort: String) throws -> (String, String) {
-    return ("", "")
+    let chars = [Character](hostPort.characters)
+    // get the index of the last colon
+    guard let lastColon = chars.reversed().index(of: ":") else {
+      throw MessageError("missing port", context: ["hostPort": hostPort])
+    }
+
+    // translate backward index to forward one
+    let portColon = lastColon.base - 1
+    var (open, close) = (0, 0)
+
+    let host: String
+    if chars[0] == "[" {
+      // find the closing ']'
+      guard let closeBracket = chars.index(of: "]") else {
+        throw MessageError("missing ']' in address", context: ["hostPort": hostPort])
+      }
+
+      switch closeBracket + 1 {
+      case chars.count:
+        // there can't be a ":" after the "]"
+        throw MessageError("missing port", context: ["hostPort": hostPort])
+      case portColon:
+        // expected
+        break
+      default:
+        // "]" is not followed by the last colon
+        if chars[closeBracket + 1] == ":" {
+          throw MessageError("too many colons", context: ["hostPort": hostPort])
+        }
+        throw MessageError("missing port", context: ["hostPort": hostPort])
+      }
+
+      host = String(chars[1..<closeBracket])
+      (open, close) = (1, closeBracket + 1) // can't be a '[' / ']' before those indices
+
+    } else {
+
+      let hostSlice = chars[0..<portColon]
+      if hostSlice.index(of: ":") != nil {
+        throw MessageError("too many colons", context: ["hostPort": hostPort])
+      }
+      if hostSlice.index(of: "%") != nil {
+        throw MessageError("missing brackets", context: ["hostPort": hostPort])
+      }
+
+      host = String(hostSlice)
+    }
+
+    if chars[open..<chars.count].index(of: "[") != nil {
+      throw MessageError("unexpected '['", context: ["hostPort": hostPort])
+    }
+    if chars[close..<chars.count].index(of: "]") != nil {
+      throw MessageError("unexpected ']'", context: ["hostPort": hostPort])
+    }
+
+    let port = String(chars[(portColon+1)..<chars.count])
+    return (host, port)
   }
 
   static func lookupPort(forService service: String, family: Family = .unknown, proto: SocketProtocol = .tcp) throws -> Int {
--- Tests/LowSocketsTests/ResolverTests.swift
+++ Tests/LowSocketsTests/ResolverTests.swift
@@ -2,6 +2,90 @@ import XCTest
 @testable import LowSockets
 
 class ResolverTests: XCTestCase {
+  // Test cases from Go net/ip_test.go
+  let splitJoinCases: [(host: String, port: String, join: String)] = [
+    ("www.google.com", "80", "www.google.com:80"),
+    ("127.0.0.1", "1234", "127.0.0.1:1234"),
+    ("::1", "80", "[::1]:80"),
+    ("fe80::1%lo0", "80", "[fe80::1%lo0]:80"),
+    ("localhost%lo0", "80", "[localhost%lo0]:80"),
+    ("", "0", ":0"),
+    ("google.com", "https%foo", "google.com:https%foo"),
+    ("127.0.0.1", "", "127.0.0.1:"),
+    ("www.google.com", "", "www.google.com:"),
+  ]
+
+  // Test cases from Go net/ip_test.go
+  let splitFailureCases: [(hostPort: String, err: String)] = [
+    ("www.google.com", "missing port"),
+    ("127.0.0.1", "missing port"),
+    ("[::1]", "missing port"),
+    ("[fe80::1%lo0]", "missing port"),
+    ("[localhost%lo0]", "missing port"),
+    ("localhost%lo0", "missing port"),
+    ("::1", "too many colons"),
+    ("fe80::1%lo0", "too many colons"),
+    ("fe80::1%lo0:80", "too many colons"),
+    ("localhost%lo0:80", "missing brackets"),
+    ("[foo:bar]", "missing port"),
+    ("[foo:bar]baz", "missing port"),
+    ("[foo]bar:baz", "missing port"),
+    ("[foo]:[bar]:baz", "too many colons"),
+    ("[foo]:[bar]baz", "unexpected '['"),
+    ("foo[bar]:baz", "unexpected '['"),
+    ("foo]bar:baz", "unexpected ']'"),
+  ]
+
+  func testJoinHostPort() {
+    let cases: [(host: String, port: String, join: String)] = [
+      ("", "", ":"),
+      ("a", "", "a:"),
+      ("a", "b", "a:b"),
+      ("a:b", "c", "[a:b]:c"),
+      ("a%b", "c", "[a%b]:c"),
+      ("a%b:c", "d", "[a%b:c]:d"),
+      ("%", "c", "[%]:c"),
+      (":", "c", "[:]:c"),
+      ("%:", "c", "[%:]:c"),
+      ("%a", "c", "[%a]:c"),
+      (":a", "c", "[:a]:c"),
+      ("%:a", "c", "[%:a]:c"),
+      ("a%", "c", "[a%]:c"),
+      ("a:", "c", "[a:]:c"),
+      ("a%:", "c", "[a%:]:c"),
+    ]
+
+    for c in cases + splitJoinCases {
+      let got = Resolver.join(host: c.host, port: c.port)
+      XCTAssertEqual(c.join, got)
+    }
+  }
+
+  func testSplitHostPort() {
+    for c in splitJoinCases {
+      do {
+        let got = try Resolver.split(hostPort: c.join)
+
+        XCTAssertEqual(c.host, got.0)
+        XCTAssertEqual(c.port, got.1)
+      } catch {
+        XCTFail("unexpected error: \(error)")
+      }
+    }
+
+    for c in splitFailureCases {
+      do {
+        _ = try Resolver.split(hostPort: c.hostPort)
+        XCTFail("expected error: \(c.hostPort)")
+      } catch {
+        let msg = "\(error)"
+        if !msg.contains(c.err) {
+          XCTFail("expected \(c.err), got \(msg)")
+        }
+      }
+    }
+  }
+
   func testLookupPort() {
     let cases: [(String, Int)] = [
       ("http", 80),
